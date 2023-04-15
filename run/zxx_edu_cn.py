5cafbda45c07f64f8bf6da78ca7af3313a7668e0
uatbo <uatbo@outlook.com>
2023-04-16 03:08:26 +0800

    修复bug

--- run/zxx_edu_cn.py
+++ run/zxx_edu_cn.py
@@ -12,22 +12,22 @@ def run():
         # 将列表保存为 YAML 文件
         with open('./configs/zxx_edu_cn.yml', 'w', encoding='utf-8') as file:
             yaml.dump(request.sinfo, file, allow_unicode=True)
+            # 清除不完整的数据
+            while (True):
+                if request.data["grade"][-1] == request.sinfo[0][0] and request.data["version"][-1] == request.sinfo[0][1] and request.data["semester"][-1] == request.sinfo[0][2]:
+                    request.data["grade"].pop()
+                    request.data["semester"].pop()
+                    request.data["category"].pop()
+                    request.data["version"].pop()
+                    request.data["executor"].pop()
+                    request.data["chapter"].pop()
+                    request.data["section"].pop()
+                    request.data["name"].pop()
+                    request.data["content"].pop()
+                else:
+                    break
         print(Exception)
 
-    # 清除不完整的数据
-    while(True):
-        if request.data["grade"][-1] == request.sinfo[0][0] and request.data["version"][-1] == request.sinfo[0][1] and request.data["semester"][-1] == request.sinfo[0][2]:
-            request.data["grade"].pop()
-            request.data["semester"].pop()
-            request.data["category"].pop()
-            request.data["version"].pop()
-            request.data["executor"].pop()
-            request.data["chapter"].pop()
-            request.data["section"].pop()
-            request.data["name"].pop()
-            request.data["content"].pop()
-        else:
-            break
 
     # 存储爬取的信息
     df = pd.DataFrame(request.data)
